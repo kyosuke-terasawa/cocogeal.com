41fd2bd22e06747d9e416a52fefee8fa5a9966e2
kyosuke terasawa <kytcu20e@gmail.com>
2020-12-05 08:48:27 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+
+<title>サイト名</title>
+
+<link rel="stylesheet" href="style.css" type="text/css" media="all" />
+<meta name="description" content="サイトの概要">
+
+
+</head>
+
+<body>
+
+<div id="container">
+
+
+<!-- ヘッダー -->
+　<div id="boxA">
+<h1><a href="サイトURL">サイト名</a></h1>
+　</div>
+<!-- /ヘッダー -->
+
+
+<!-- メイン（記事一覧部分） -->
+　<div id="boxB">
+
+<div class="entry">
+カテゴリー名：<a href="記事１へのリンク">記事１タイトル</a><br />
+本文本文本文本文本文本文本文本文本文本文本文本文本文本文<br />
+</div>
+
+<div class="entry">
+カテゴリー名：<a href="記事２へのリンク">記事２タイトル</a><br />
+本文本文本文本文本文本文本文本文本文本文本文本文本文本文<br />
+</div>
+
+<div class="entry">
+カテゴリー名：<a href="記事３へのリンク">記事３タイトル</a><br />
+本文本文本文本文本文本文本文本文本文本文本文本文本文本文<br />
+</div>
+
+<div class="entry">
+カテゴリー名：<a href="記事４へのリンク">記事４タイトル</a><br />
+本文本文本文本文本文本文本文本文本文本文本文本文本文本文<br />
+</div>
+
+<div class="entry">
+カテゴリー名：<a href="記事５へのリンク">記事５タイトル</a><br />
+本文本文本文本文本文本文本文本文本文本文本文本文本文本文<br />
+</div>
+
+
+
+　</div>
+<!-- /メイン -->
+
+
+
+<!-- サイドバー -->
+　<div id="boxC">
+　<img src="images/panda.jpg">
+　</div>
+<!-- /サイドバー -->
+
+
+
+<!-- フッター -->
+　<div id="boxD">
+　Copyright © 2013 はじめてのWordPressまとめ
+　</div>
+<!-- /フッター -->
+
+
+</div>
+
+
+</body>
+</html>
+
